dbe45bb3e2c5361de9141b781df5f273fcd9ab97
zsiki <siki1958@gmail.com>
2018-07-05 05:20:59 -0400

    init for Hungarian messages

---
+++ i18n/en.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>Label2Attr</name>
+    <message>
+        <location filename="label2attr.py" line="159"/>
+        <source>&amp;Label to Attribute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr.py" line="99"/>
+        <source>Label to Attribute Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr.py" line="106"/>
+        <source>Label to Attribute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr.py" line="212"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr.py" line="141"/>
+        <source>Point not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr.py" line="206"/>
+        <source>Please set config parameters!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr.py" line="212"/>
+        <source>Please select a single feature in target layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Label2AttrDialogBase</name>
+    <message>
+        <location filename="label2attr_dialog_base.ui" line="14"/>
+        <source>Label to Attribute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr_dialog_base.ui" line="42"/>
+        <source>Point Label Layer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr_dialog_base.ui" line="65"/>
+        <source>Target Layer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr_dialog_base.ui" line="88"/>
+        <source>Target Column:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr_dialog_base.ui" line="121"/>
+        <source>Label Column:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr_dialog_base.ui" line="134"/>
+        <source>Tolerance:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr_dialog_base.ui" line="147"/>
+        <source>5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="label2attr_dialog_base.ui" line="160"/>
+        <source>map units</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
