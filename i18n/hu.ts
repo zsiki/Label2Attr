d046d05c9cecffc8f05e9be4ca700ef725a8aac4
zsiki <siki@agt.bme.hu>
2018-07-05 11:28:13 +0200

    Hungarian translation finished

--- i18n/hu.ts
+++ i18n/hu.ts
@@ -1,41 +1,42 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
+<!DOCTYPE TS>
+<TS version="2.0" language="hu_HU">
 <context>
     <name>Label2Attr</name>
     <message>
         <location filename="label2attr.py" line="159"/>
         <source>&amp;Label to Attribute</source>
-        <translation type="unfinished"></translation>
+        <translation>Címke attribútumba</translation>
     </message>
     <message>
         <location filename="label2attr.py" line="99"/>
         <source>Label to Attribute Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Címke attribútumba beállítások</translation>
     </message>
     <message>
         <location filename="label2attr.py" line="106"/>
         <source>Label to Attribute</source>
-        <translation type="unfinished"></translation>
+        <translation>Címke attribútumba</translation>
     </message>
     <message>
         <location filename="label2attr.py" line="212"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Figyelmeztetés</translation>
     </message>
     <message>
         <location filename="label2attr.py" line="141"/>
         <source>Point not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem találtam pontot</translation>
     </message>
     <message>
         <location filename="label2attr.py" line="206"/>
         <source>Please set config parameters!</source>
-        <translation type="unfinished"></translation>
+        <translation>Állítsa be a konfiguráció paramétereket!</translation>
     </message>
     <message>
         <location filename="label2attr.py" line="212"/>
         <source>Please select a single feature in target layer</source>
-        <translation type="unfinished"></translation>
+        <translation>Szelektáljon egy elemet a cél rétegen</translation>
     </message>
 </context>
 <context>
@@ -43,42 +44,42 @@
     <message>
         <location filename="label2attr_dialog_base.ui" line="14"/>
         <source>Label to Attribute</source>
-        <translation type="unfinished"></translation>
+        <translation>Címke attribútumba</translation>
     </message>
     <message>
         <location filename="label2attr_dialog_base.ui" line="42"/>
         <source>Point Label Layer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Pont címke réteg:</translation>
     </message>
     <message>
         <location filename="label2attr_dialog_base.ui" line="65"/>
         <source>Target Layer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cél réteg:</translation>
     </message>
     <message>
         <location filename="label2attr_dialog_base.ui" line="88"/>
         <source>Target Column:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cél oszlop:</translation>
     </message>
     <message>
         <location filename="label2attr_dialog_base.ui" line="121"/>
         <source>Label Column:</source>
-        <translation type="unfinished"></translation>
+        <translation>Címke oszlop</translation>
     </message>
     <message>
         <location filename="label2attr_dialog_base.ui" line="134"/>
         <source>Tolerance:</source>
-        <translation type="unfinished"></translation>
+        <translation>Tolerancia:</translation>
     </message>
     <message>
         <location filename="label2attr_dialog_base.ui" line="147"/>
         <source>5</source>
-        <translation type="unfinished"></translation>
+        <translation>5</translation>
     </message>
     <message>
         <location filename="label2attr_dialog_base.ui" line="160"/>
         <source>map units</source>
-        <translation type="unfinished"></translation>
+        <translation>térképi egység</translation>
     </message>
 </context>
 </TS>
